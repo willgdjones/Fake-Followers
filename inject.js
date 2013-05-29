c91af1f10cde9f811dcac8ef40f2f262b03b7139
Will Jones <williamgdjones@gmail.com>
2013-05-30 00:56:07 +0100

    Added loading screen zzz

--- inject.js
+++ inject.js
@@ -2,26 +2,30 @@ if (typeof jQuery === 'undefined') {
   alert("Sad face :(")
 } else {
 
-	// Run Javascript when document loads
+	// Create loading screen 'zzz' as a new list element with .extra class in the .stats div when the page is loading
+	// Uses inline CSS to fit the design of twitter
+	$('.stats').append('<li class="extra"><a class="js-nav"><strong>zzz</strong></a></li>');
+
+	// Run main code when twitter page is ready
   jQuery(document).ready(function($){
 
   	//Get twitter username from URL
     var username = window.location.pathname.replace('/','');
     console.log(username);
 
-    // Send HTTP get to Twitter Audit with this username
+    // Send HTTP get request to Twitter Audit with this username
     $.get("http://www.twitteraudit.com/"+username, function(data) {
 
     	// Find Twitter Audit score percentage from within the retrieved data using jQuery
       var percentage = $(data).find('.percentage').html();
 
-      // If a Twitter Audit hasn't been made yet
-     	if (percentage === 'undefined') {
-      	$('.stats').append('<li><a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + ':(' + '</strong> Real</a></li>');
+      // If a Twitter Audit hasn't been made yet, replace loading screen with a sad face
+     	if (!percentage) {
+      	$('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + ':(' + '</strong> Real</a>')
       } else {
       
-	      // Inject this percentage as a new list element in the .stats div, using inline CSS to fit the design of twitter
-	      $('.stats').append('<li><a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + percentage + '</strong> Real</a></li>');
+	      // Otherwise replace loading screen with Twitter Audit percentage
+	      $('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + percentage + '</strong> Real</a>')
 	    }
 
     });
