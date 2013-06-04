9d26cecf3f52a37a4efede0d27c6bd6f96ea2573
Will Jones <williamgdjones@gmail.com>
2013-06-04 18:20:18 +0100

    Display % of fake followers instead of real followers

--- inject.js
+++ inject.js
@@ -19,16 +19,21 @@ if (typeof jQuery === 'undefined') {
     // Send HTTP get request to Twitter Audit with this username
     $.get("http://www.twitteraudit.com/"+username, function(data) {
 
-    	// Find Twitter Audit score percentage from within the retrieved data using jQuery
+    	// Find Twitter Audit score percentage (string) from within the retrieved data using jQuery
       var percentage = $(data).find('.percentage').html();
 
       // If a Twitter Audit hasn't been made yet, replace loading screen with a sad face
      	if (!percentage) {
-      	$('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + ':(' + '</strong> Real</a>')
+      	$('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + ':(' + '</strong></a>')
       } else {
       
-	      // Otherwise replace loading screen with Twitter Audit percentage
-	      $('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + percentage + '</strong> Real</a>')
+	      // Otherwise, find out fake percentage
+        var fakePercentage = 100 - parseInt(percentage);
+        var fakePercentageString = fakePercentage.toString() + '%'
+
+
+        // and replace loading screen with Twitter Audit fake percentage
+	      $('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + fakePercentageString + '</strong> Fake</a>')
 	    }
 
     });
