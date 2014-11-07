e8fa2b5888fef533e16944ec6915f55ce4f5d731
William Jones <williamgdjones@gmail.com>
2014-11-07 16:56:05 +0000

    Update for new Twitter layout. Hack around CSRF

--- inject.js
+++ inject.js
@@ -1,5 +1,7 @@
 
 // If jQuery is not defined, alert a sad face
+
+console.log("This is working")
 if (typeof jQuery === 'undefined') {
   alert("Sad face :(")
 } 
@@ -15,8 +17,7 @@ if (typeof jQuery === 'undefined') {
 
 	// Create loading screen with our icon as a new list element with .extra class in the .stats div when the page is loading
 	// Uses inline CSS to fit the design of twitter
-	$('.stats').append('<li class="extra"><a class="js-nav" href="#" ><img src="' + loadScreen + '""></a></li>');
-
+	$('.ProfileNav-list').append('<li id="fonzfakefollower" class="ProfileNav-item ProfileNav-item--follower"><a class="ProfileNav-stat ProfileNav-stat--link u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor" data-nav="followers"><span class="ProfileNav-label">Fake Followers</span><span class="ProfileNav-value" data-is-compact="true"><img src="' + loadScreen + '""></span></a></li>')
 
 	// Run main code when twitter page is ready
   jQuery(document).ready(function($){
@@ -25,37 +26,76 @@ if (typeof jQuery === 'undefined') {
     var username = window.location.pathname.replace('/','');
 
     // Send HTTP get request to Twitter Audit with this username
-    $.get("http://www.twitteraudit.com/"+username, function(data) {
+    $.ajax({
+      type: "GET",
+      dataType: 'json',
+      url: 'https://www.twitteraudit.com/' + username,
+      success: function(data) {console.log(data)},
+      error: function(error) {
+        var percentage = $(error.responseText).find('.percentage').html();
+        console.log(percentage);
 
-    	// Find Twitter Audit score percentage (string) from within the retrieved data using jQuery
-      var percentage = $(data).find('.percentage').html();
-
-      // If a Twitter Audit hasn't been made yet, replace loading screen with a sad face
-     	if (!percentage) {
-      	$('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + ':(' + '</strong></a>')
+        if (!percentage) {
+        $('#fonzfakefollower').html('<a class="ProfileNav-stat ProfileNav-stat--link u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor" href="https://www.twitteraudit.com/'+username+'"><span class="ProfileNav-label">Fake Followers</span><span class="ProfileNav-value" data-is-compact="true"><strong>' + 'Find out!' + '</strong></a>')
       } else {
       
-	      // Otherwise, find out fake percentage
+        // Otherwise, find out fake percentage
         var fakePercentage = 100 - parseInt(percentage);
         var fakePercentageString = fakePercentage.toString() + '%';
+        console.log(fakePercentage)
 
         // and replace loading screen with Twitter Audit fake percentage
 
         // If the fake percentage is more than or equal to 50%, display text in red.
         if (fakePercentage >= 50) {
-          $('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong style="color:red">' + fakePercentageString + '</strong> Fake</a>'); 
+          $('#fonzfakefollower').html('<a class="ProfileNav-stat ProfileNav-stat--link u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor" href="https://www.twitteraudit.com/'+username+'"><span class="ProfileNav-label">Fake Followers</span><span class="ProfileNav-value" data-is-compact="true"><strong style="color:red">' + fakePercentageString + '</strong></a>')
+          // $('.stats .extra').html('<a class="js-nav" href="https://www.twitteraudit.com/'+username+'"><strong style="color:red">' + fakePercentageString + '</strong> Fake</a>'); 
         } else {
 
           // Otherwise, display normally.
-          $('.stats .extra').html('<a class="js-nav" href="http://www.twitteraudit.com/'+username+'"><strong>' + fakePercentageString + '</strong> Fake</a>');
-        }
+          $('#fonzfakefollower').html('<a class="ProfileNav-stat ProfileNav-stat--link u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor" href="https://www.twitteraudit.com/'+username+'"><span class="ProfileNav-label">Fake Followers</span><span class="ProfileNav-value" data-is-compact="true"><strong>' + fakePercentageString + '</strong></a>')
+          // $('.stats .extra').html('<a class="js-nav" href="https://www.twitteraudit.com/'+username+'"><strong>' + fakePercentageString + '</strong> Fake</a>');
         
-	    }
 
-    });
+          }
+        } 
+      }
+    })
 
-  })
 
+    })
 }
+    // $.get("https://www.twitteraudit.com/"+username, function(data) {
+    //   console.log(data)
+    // 	// Find Twitter Audit score percentage  (string) from within the retrieved data using jQuery
+    //   var percentage = $(data).find('.percentage').html();
+
+      // If a Twitter Audit hasn't been made yet, replace loading screen with a sad face
+     	// if (!percentage) {
+      // 	$('.stats .extra').html('<a class="js-nav" href="https://www.twitteraudit.com/'+username+'"><strong>' + ':(' + '</strong></a>')
+      // } else {
+      
+	     //  // Otherwise, find out fake percentage
+      //   var fakePercentage = 100 - parseInt(percentage);
+      //   var fakePercentageString = fakePercentage.toString() + '%';
+
+      //   // and replace loading screen with Twitter Audit fake percentage
+
+      //   // If the fake percentage is more than or equal to 50%, display text in red.
+      //   if (fakePercentage >= 50) {
+      //     $('.stats .extra').html('<a class="js-nav" href="https://www.twitteraudit.com/'+username+'"><strong style="color:red">' + fakePercentageString + '</strong> Fake</a>'); 
+      //   } else {
+
+      //     // Otherwise, display normally.
+      //     $('.stats .extra').html('<a class="js-nav" href="https://www.twitteraudit.com/'+username+'"><strong>' + fakePercentageString + '</strong> Fake</a>');
+    //     }
+        
+	   //  }
+
+    // });
+
+//   })
+
+// }
 
 
